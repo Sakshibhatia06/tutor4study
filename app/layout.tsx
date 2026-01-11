b32c755354985bce25fc6677924cd9733da9fb61
SAKSHI BHATIA <145453542+Sakshibhatia06@users.noreply.github.com>
2026-01-11 19:25:27 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,10 +2,12 @@ import './globals.css';
 import Navbar from './components/Navbar/index';
 import Footer from './components/Footer/Footer';
 import WhatsAppButton from './components/whatsappButton/WhatsAppButton';
+import Script from 'next/script';
 
 export const metadata = {
   title: 'Tutor4Study',
-  description: 'Tutor4Study offers expert online tutoring for school subjects, college courses, and competitive exams. Learn from experienced tutors with flexible schedules and personalized guidance.',
+  description:
+    'Tutor4Study offers expert online tutoring for school subjects, college courses, and competitive exams.',
 };
 
 export default function RootLayout({
@@ -16,18 +18,25 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-     <script async src="https://www.googletagmanager.com/gtag/js?id=G-2ZY5NEHH89"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-2ZY5NEHH89');
-</script>
+        {/* Google site verification */}
         <meta
           name="google-site-verification"
           content="BI8jQ255K9NVW-IK9E7BAab8jYsBOqX9sCREOBOMurE"
         />
+
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-2ZY5NEHH89"
+          strategy="afterInteractive"
+        />
+        <Script id="ga-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-2ZY5NEHH89');
+          `}
+        </Script>
       </head>
 
       <body>
